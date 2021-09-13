14948346522abe8043948598dfcc79ac31a56e6b
keanesense8 <tengjuny0123@gmail.com>
2021-09-14 00:58:39 +0800

    init

---
+++ src/main/java/com/keane/cloud/jvmpromethusgrafana/JvmPromethusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.keane.cloud.jvmpromethusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmPromethusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmPromethusGrafanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keane/cloud/jvmpromethusgrafana/JvmPromethusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keane.cloud.jvmpromethusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmPromethusGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
